63d671626f07069f70fc1f3da430b1c7f62dccaa
Achal Kavitkar <achalkavitkar@gmail.com>
2021-09-13 16:17:24 +0530

    added variable c

--- diff.java
+++ diff.java
@@ -2,7 +2,7 @@ public class diff{
 	public static void main(String[] args){
 		int a=5;
 		int b=6;
-		
-		System.out.println("Diff is"+a-b);
+		int c=a+b;
+		System.out.println("Diff is"+c);
 	}
 }
